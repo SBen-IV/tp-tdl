c1d76bbf88178a7f057f3d53d61f84e6bcf1f1e0
Gonzalo Calderón <79221793+gcaldev@users.noreply.github.com>
2022-05-29 16:44:56 -0300

    App control empezado y agregue un par de rutas

--- main.go
+++ main.go
@@ -1,10 +1,78 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
+
+type AppController struct {
+	users      []*User
+	auctions   []*Auction
+	lastUserID int
+}
+
+func (app *AppController) authUser(currentUser *User) bool {
+	//To be implemented
+	return true
+}
+
+// Same code in FindUserByID & FindAuctionByID
+func (app *AppController) FindUserByID(id string) *User {
+	var foundUser *User
+	users := app.GetRegisteredUsers()
+	for _, user := range users {
+		if user.ID == id {
+			foundUser = user
+			break
+		} else {
+			foundUser = nil
+		}
+	}
+	if foundUser == nil {
+		panic("User is invalid")
+	}
+	return foundUser
+}
+
+func (app *AppController) FindAuctionByID(id string) *Auction {
+	auctions := app.GetRegisteredAuctions()
+	var foundAuction *Auction
+	for _, auction := range auctions {
+		if auction.ID == id {
+			foundAuction = auction
+			break
+		} else {
+			foundAuction = nil
+		}
+	}
+	if foundAuction == nil {
+		panic("Auction is invalid")
+	}
+	return foundAuction
+}
+
+func (app *AppController) GetCurrentUserID() int {
+	return app.lastUserID
+}
+
+func (app *AppController) IncrementUserID() {
+	app.lastUserID++
+}
+
+func (app *AppController) GetRegisteredUsers() []*User {
+	return app.users
+}
+
+func (app *AppController) AddNewUser(newUser *User) {
+	app.users = append(app.users, newUser)
+}
+
+func (app *AppController) GetRegisteredAuctions() []*Auction {
+	return app.auctions
+}
 
 type User struct {
 	ID        string `json:"id"`
@@ -16,14 +84,11 @@ type User struct {
 
 type Auction struct {
 	ID           string  `json:"id"`
+	Seller       *User   `json:"seller"`
 	Participants []*User `json:"participants"`
 	currentOffer int     `json:"currentoffer"`
 	isPublic     bool    `json:"ispublic"`
-}
-
-type AppController struct {
-	users      []*User
-	lastUserID int
+	hasStarted   bool    `json:"-"`
 }
 
 type AuctionType interface {
@@ -31,19 +96,76 @@ type AuctionType interface {
 	addParticipant() int
 }
 
-func (*AppController) authUser(currentUser *User) bool {
-	return true
+var app AppController
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var user User
+	json.NewDecoder(r.Body).Decode(&user)
+	user.ID = strconv.Itoa(app.GetCurrentUserID())
+	app.IncrementUserID()
+	app.AddNewUser(&user)
+	json.NewEncoder(w).Encode(user)
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	users := app.GetRegisteredUsers()
+	json.NewEncoder(w).Encode(users)
+}
+
+func createAuction(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var auction Auction
+	json.NewDecoder(r.Body).Decode(&auction)
+	if auction.Participants == nil {
+		auction.Participants = []*User{auction.Seller}
+	}
+	auctions := app.GetRegisteredAuctions()
+	json.NewEncoder(w).Encode(auctions)
+}
+
+func getAuctions(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	auctions := app.GetRegisteredAuctions()
+	json.NewEncoder(w).Encode(auctions)
+}
+
+func joinAuction(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	foundAuction := app.FindAuctionByID(params["auctionid"])
+	foundUser := app.FindUserByID(params["userid"])
+	foundAuction.Participants = append(foundAuction.Participants, foundUser)
+}
+
+func updateAuctionOffer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	auctionId := params["auctionid"]
+	rawOffer := params["newoffer"]
+	newOffer, err := strconv.Atoi(rawOffer)
+	if err != nil {
+		panic("Received offer is invalid")
+	}
+	auctionToUpdate := app.FindAuctionByID(auctionId)
+	if auctionToUpdate.currentOffer < newOffer {
+		auctionToUpdate.currentOffer = newOffer
+	}
 }
 
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/users", getUsers).Methods("GET")
-	r.HandleFunc("/users/{id}", getUser).Methods("GET")
+	app = AppController{[]*User{}, []*Auction{}, 0}
+
 	r.HandleFunc("/users", createUser).Methods("POST")
-	r.HandleFunc("/user/auth", authUser).Methods("POST")
-	r.HandleFunc("/users/{id}", updateUser).Methods("PUT")
-	r.HandleFunc("/users/{id}", deleteUser).Methods("DELETE")
+	r.HandleFunc("/users", getUsers).Methods("GET")
+	r.HandleFunc("/auctions", createAuction).Methods("POST")
+	r.HandleFunc("/auctions", getAuctions).Methods("GET")
+	r.HandleFunc("/auctions/auctionid={auctionid}&userid={userid}", joinAuction).Methods("PUT")
+	r.HandleFunc("/auctions/auctionid={auctionid}&userid={userid}&newoffer={newoffer}", updateAuctionOffer).Methods("PUT")
+	r.HandleFunc("/auctions", createAuction).Methods("POST")
 
 	http.ListenAndServe(":8000", r)
 }
